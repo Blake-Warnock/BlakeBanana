812008aa22ec333ab718fdd60f91c0532f914c85
Blake Warnock <blake.warnock15@gmail.com>
2023-03-06 11:18:10 -0600

    Add project files.

---
+++ BlakeBananaWeb/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using BlakeBananaWeb.Data;
+using BlakeBananaWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlakeBananaWeb.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Products> products = _context.Products;
+            return View(products);
+        }
+    }
+}
---
+++ BlakeBananaWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿
+using BlakeBananaWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlakeBananaWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Products> Products { get; set; }
+    }
+}
