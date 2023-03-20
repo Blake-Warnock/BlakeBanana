2ada32c6ed06e8be023d90f85da19aa1ebd8dd27
Blake Warnock <blake.warnock15@gmail.com>
2023-03-20 09:50:36 -0500

    Adds users to database

--- BlakeBananaWeb/Data/ApplicationDbContext.cs
+++ BlakeBananaWeb/Data/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
 ﻿
 using BlakeBananaWeb.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BlakeBananaWeb.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
